b7565a1fd673d257481a23d5dd7dce080bbdf242
jonathan.kablan <jonathan.kablan@gmail.com>
2017-02-20 01:32:12 +0100

    install upload prestashop

---
+++ prestashop/config/settings.inc.php
@@ -0,0 +1,14 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'bd_prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'password');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'RT85z3CqL2FwqBIMqZAQZoOWmiYxSfZHYKTctoTM7Ev6K4Chqmi5oGIs');
+define('_COOKIE_IV_', 'FPmrA7nt');
+define('_PS_CREATION_DATE_', '2017-02-20');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.11');
